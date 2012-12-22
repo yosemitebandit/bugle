25f4a7683817a1582b8f2f03ae83482abe2e6fb9
Matt Ball <yosemitebandit@gmail.com>
2012-12-22 01:13:46 -0800

    adds testing basics

---
+++ tests/test_yaml.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+''' testing yaml settings
+'''
+
+import unittest
+
+class YamlTest(unittest.TestCase):
+
+    def test_yaml_extraction(self):
+        pass
